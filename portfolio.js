58b4db199cb273760e1aafdcccf7dd752a00902e
bunhongvoeurn123-collab <bunhongvoeurn123@gmail.com>
2026-07-11 21:23:34 +0700

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,61 @@
+
+
+const hong = document.getElementById("hong");
+
+const menu = document.getElementById("menu");
+
+
+hong.addEventListener("click",()=>{
+
+    menu.classList.toggle("active");
+
+});
+
+
+const themeBtn = document.getElementById("theme-btn");
+
+
+themeBtn.addEventListener("click",()=>{
+
+    document.body.classList.toggle("dark");
+
+});
+
+const navbar = document.getElementById("navbar");
+
+
+window.addEventListener("scroll",()=>{
+
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("small");
+
+    }else{
+
+        navbar.classList.remove("small");
+
+    }
+
+
+});
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach((reveal) => {
+
+        const windowHeight = window.innerHeight;
+
+        const revealTop = reveal.getBoundingClientRect().top;
+
+        if (revealTop < windowHeight - 100) {
+
+            reveal.classList.add("active");
+
+        }
+
+    });
+
+});
